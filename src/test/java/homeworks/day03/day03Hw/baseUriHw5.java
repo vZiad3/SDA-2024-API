3b5600e6bab51b94a2a95bd2df4a4122d7f33077
Ziad alshammary <ziad53@Ziads-MacBook-Pro.local>
2024-04-27 20:07:07 +0300

    ed

--- src/test/java/homeworks/day03/day03Hw/baseUriHw5.java
+++ src/test/java/homeworks/day03/day03Hw/baseUriHw5.java
@@ -2,5 +2,24 @@ package homeworks.day03.day03Hw;
 
 public class baseUriHw5 {
 
+//{
+//    "id": 0,
+//    "category": {
+//        "id": 0,
+//        "name": "string"
+//    },
+//    "name": "doggie",
+//    "photoUrls": [
+//        "string"
+//    ],
+//    "tags": [
+//        {
+//            "id": 0,
+//            "name": "string"
+//        }
+//    ],
+//    "status": "available"
+//}
 
+    private Integer id;
 }
